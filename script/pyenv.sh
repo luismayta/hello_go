e10f2f4a10613ffc463b5ca6ffc00b06c6f2e825
@slovacus <slovacus@gmail.com>
2016-08-14 21:15:01 -0500

    [#2] Fixed make pyenv

--- script/pyenv.sh
+++ script/pyenv.sh
@@ -5,9 +5,4 @@
 
 cd "${ROOT_DIR}"
 
-pyenv versions | grep -q "${PYENV_NAME}"
-response=$?
-
-if [[ ! ${response} -eq '0' ]]; then
-    pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" > /dev/null
-fi
+pyenv virtualenv "${PYTHON_VERSION}" "${PYENV_NAME}" > /dev/null
