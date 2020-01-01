5f7fd757c41a410ec22cabbe72e07f6f35a11a66
@slovacus <slovacus@gmail.com>
2020-01-01 15:28:20 -0500

    (#9) chore: Implement actions for make

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -2,45 +2,19 @@ test.help:
 	@echo '    Tests:'
 	@echo ''
 	@echo '        test                      show help'
-	@echo '        test run={module}         Run module test'
 	@echo '        test.all   	             Run all module test'
-	@echo '        test.picked               run test only commit'
 	@echo '        test.lint                 Run all pre-commit'
-	@echo '        test.lint.docker          Run all pre-commit in docker'
-	@echo '        test.syntax               Run all syntax in code'
-	@echo '        test.validate             Run all validation fixture dead in code'
 	@echo ''
 
-test: clean
+test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
 	@if [ -z "${run}" ]; then \
 		make test.help;\
 	fi
-	@if [ -n "${run}" ]; then \
-		$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
-			"$(PIPENV_RUN) py.test ${run}";\
-	fi
 
-test.all: clean
+test.all:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
-		"$(PIPENV_RUN) pytest"
+	$(docker-test-run) bash -c "zunit"
 
-test.picked: clean
-	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
-		"$(PIPENV_RUN) pytest --picked"
-
-test.validate: clean
-	@echo $(MESSAGE) Running tests validation fixture $(END)
-	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_APP) bash -c \
-		"$(PIPENV_RUN) pytest --dead-fixtures"
-
-test.lint: clean
+test.lint:
 	$(PIPENV_RUN) pre-commit run --all-files --verbose
-
-test.lint.docker: clean
-	$(DOCKER_COMPOSE_TEST) run --rm $(SERVICE_CHECK) bash -c \
-		"$(PIPENV_RUN) pre-commit run --all-files --verbose"
-
-test.syntax: clean
-	@echo $(MESSAGE) Running tests $(END)
